42e3ed33cd1fd8b8e099a7b152cadb59a00ed804
David Chiu <tr.ywchiu@gmail.com>
2018-09-17 16:48:55 +0800

    api app

---
+++ Dashboard/app2.py
@@ -0,0 +1,27 @@
+﻿from flask import Flask, jsonify
+import random
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def api():
+    dt   = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    data = random.randint(50,900)
+    return jsonify({'dt':dt, 'data':data})
+
+@app.route("/hello/<name>")
+def api2(name):
+    return 'hello, ' + name
+
+@app.route("/data2/", methods=['POST'])
+def postapi2(name):
+    if request.method=='POST':
+        area=request.form['area']
+        return jsonify({'data':area})
+
+@app.route("/data")
+def data():
+    return "<html><body> <a href='https://www.largitdata.com'> LargitData </a></body></html>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = 9999)
