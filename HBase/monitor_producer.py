c51aebfb8dab093e7028945d2ae8ff9b4e35b5be
David Chiu <tr.ywchiu@gmail.com>
2018-09-17 16:11:30 +0800

    monitor producer

---
+++ HBase/monitor_producer.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+import time, json
+from datetime import datetime
+import os
+import psutil
+from kafka import KafkaConsumer, KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+
+while True:
+    dic = {}
+    dic['tm'] = int(time.mktime(datetime.now().timetuple()))
+    dic['memory'] = psutil.virtual_memory().percent
+    d = json.dumps(dic)
+
+    producer.send('system_monitor',d)
+    time.sleep(1)
+    print dic
